d606a92f324f07b8d2aa1eed12377fbeb7be58ab
Ollie Cee <olliecee.mail@gmail.com>
2020-03-18 17:13:42 -0400

    Redeploy

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 import 'core-js';
 import 'regenerator-runtime/runtime';
 import express from 'express';
-import cors from './middlewares/cors'; // Custom middleware
+// import cors from './middlewares/cors'; // Custom middleware
 
 import { getSummonerStats } from './controllers/LeagueOfLegends.controller';
 
 // Declarations
 const app = express();
 const port = process.env.PORT;
-const whitelist = ['localhost', 'localhost:3000', 'https://olliecee.com'];
+// const whitelist = ['localhost', 'localhost:3000', 'https://olliecee.com'];
 
 // Middlewares
 // app.use(cors({ whitelist }));
