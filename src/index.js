34c304be4d6e73b7d9261f2d14c64df8bd5fdba2
Ollie Cee <olliecee.mail@gmail.com>
2020-03-18 04:45:14 -0400

    fixed healthcheck

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const corsOptions = {
 app.use(cors())
 
 // Routes
-app.get('/asd', (req, res) => res.send('Hello'))
+app.get('/', (req, res) => res.send('Hello'))
 app.get('/summoner/:summoner_name', getSummonerStats)
 
 // Launching the service
