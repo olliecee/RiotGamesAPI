5b7b3223be08996cfb0f4a89da1df5451371836f
Ollie Cee <olliecee.mail@gmail.com>
2020-03-18 04:19:03 -0400

    Added healthcheck

---
+++ LeagueStatsServer/index.js
@@ -0,0 +1,31 @@
+// Imports
+import express from 'express';
+import cors from 'cors';
+
+import { getSummonerStats } from './controllers/LeagueOfLegends.controller';
+
+// Declarations
+const app = express()
+const port = process.env.PORT;
+
+// Settings
+const whitelist = ['http://localhost', 'https://olliecee.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Middleware
+app.use(cors())
+
+// Routes
+app.get('/', (req, res) => res.send('Health check'))
+app.get('/summoner/:summoner_name', getSummonerStats)
+
+// Launching the service
+app.listen(port, () => console.log(`Back end server listening on port ${port}!`))
